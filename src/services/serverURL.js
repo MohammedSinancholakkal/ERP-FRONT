20569fbad5040a54e5feddd4c7316293cb9990cf
MohammedSinancholakkal <sinansibi@gmail.com>
2025-11-26 10:06:05 +0530

    new change

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,4 +1,5 @@
 // export const serverURL = "http://localhost:5000/api"; 
+// export const serverURL = "https://erp-server-gi4d.onrender.com";
 export const serverURL = "https://erp-server-gi4d.onrender.com"; 
 
 
