de6e2c82a59f395e5c5f2d129b108b00df421441
MohammedSinancholakkal <sinansibi@gmail.com>
2025-11-27 13:52:56 +0530

    limit

--- src/services/commonAPI.js
+++ src/services/commonAPI.js
@@ -1,29 +1,20 @@
-
 // import axios from "axios";
 
-// export const commonAPI = async (method, url, body = {}, isFormData = false) => {
-//   try {
-//     let headers = {};
+// export const commonAPI = async (httpRequest, url, reqBody, reqHeader) => {
+//   const reqConfig = {
+//     method: httpRequest,
+//     url,
+//     data: reqBody,
+//     headers: reqHeader
+//       ? reqHeader
+//       : {
+//           "Content-Type": "application/json",
+//         },
+//   };
 
-//     // Auto-detect FormData OR when isFormData = true
-//     if (body instanceof FormData || isFormData) {
-//       headers["Content-Type"] = "multipart/form-data";
-//     } else {
-//       headers["Content-Type"] = "application/json";
-//     }
-
-//     const response = await axios({
-//       method,
-//       url,
-//       data: body,
-//       headers,
-//     });
-
-//     return response;
-//   } catch (error) {
-//     console.error("API ERROR:", error);
-//     return error.response || { status: 500, data: { message: "Server Error" } };
-//   }
+//   return await axios(reqConfig)
+//     .then((res) => res)
+//     .catch((err) => err);
 // };
 
 
@@ -34,7 +25,10 @@ export const commonAPI = async (method, url, body = {}, isFormData = false) => {
   try {
     let headers = {};
 
-    if (!(body instanceof FormData)) {
+    // Auto-detect FormData OR when isFormData = true
+    if (body instanceof FormData || isFormData) {
+      headers["Content-Type"] = "multipart/form-data";
+    } else {
       headers["Content-Type"] = "application/json";
     }
 
