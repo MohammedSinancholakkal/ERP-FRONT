91c83e6fa3c028918fb8d9e8fff8d9bc6507353e
MohammedSinancholakkal <sinansibi@gmail.com>
2025-12-01 11:24:45 +0530

    update

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,4 +1,5 @@
 // export const serverURL = "http://localhost:8080/api"; 
 // export const serverURL = "https://erp-server-gi4d.onrender.com";
-export const serverURL = "https://erp-server-production-8d69.up.railway.app/api";
+// export const serverURL = "https://erp-server-production-8d69.up.railway.app";
+export const serverURL = import.meta.env.VITE_API_URL;
 
